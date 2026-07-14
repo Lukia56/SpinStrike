62ab092f1f0a6f8a9321b8ea2bee90c14f5b0338
Lukia56 <kky56saqqe@gmail.com>
2026-07-14 15:20:49 +0900

    CSVの読み込み処理を一任する関数追加

---
+++ Utility/CsvLoader.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "FromCsv.h"
+
+namespace Data
+{
+	namespace Csv
+	{
+		template <typename T>
+		std::vector<T> LoadCsvAs(const std::string& path)
+		{
+			Table table = LoadRawCsv(path);
+
+			if (table.empty())
+			{
+				assert(false && "Data::Csv // データテーブルが空です");
+				return {};
+			}
+
+			std::vector<Row> rows = ToRows(table);
+
+			if (rows.empty())
+			{
+				assert(false && "Data::Csv // 行データへの変換に失敗しました");
+				return {};
+			}
+
+			std::vector<T> result;
+			result.reserve(rows.size());
+			
+			// 構造体に変換する
+			for (const auto& row : rows)
+			{
+				result.emplace_back(FromCsv<T>::Binding(row));
+			}
+
+			return result;
+		}
+	}
+}
