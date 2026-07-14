81276b5ae22dfe7f9e580174dd63b2e09c15b56d
Lukia56 <kky56saqqe@gmail.com>
2026-07-14 13:25:47 +0900

    CSVを構造体に変換する処理実装

---
+++ Utility/FromCsv.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Data.h"
+#include "DataConvert.h"
+
+struct Param
+{
+	int count = -1;
+	int hp = -1;
+	bool flag = false;
+	std::string text = "";
+};
+
+namespace Data
+{
+	namespace Csv
+	{
+		template <typename T>
+		struct FromCsv
+		{
+			static T Binding(const Row& row)
+			{
+				static_assert(sizeof(T) == 0, "Data::Csv::FromCsv // ’è‹`‚³‚ê‚Ä‚¢‚È‚¢Œ^‚ÉŠ„‚è“–‚Ä‚Å‚«‚Ü‚¹‚ñ");
+			}
+		};
+
+		template <>
+		struct FromCsv<Param>
+		{
+			static Param Binding(const Row& row)
+			{
+				Param param;
+				param.count = Convert<int>(row.at("header1"));
+				param.hp = Convert<int>(row.at("header2"));
+				param.flag = Convert<bool>(row.at("header3"));
+				param.text = Convert<std::string>(row.at("header4"));
+				return param;
+			}
+		};
+	}
+}
