74cbe4bba8759befd52e6f9b6a18932d32fed514
Lukia56 <kky56saqqe@gmail.com>
2026-08-12 17:58:16 +0900

    UIの基底オブジェクト実装

---
+++ Sources/World/UI/UIObject.cpp
@@ -0,0 +1,10 @@
+#include "UIObject.h"
+#include "../Component/RectTransform.h"
+
+UIObject::UIObject() :
+	GameObject(std::make_unique<RectTransform>(this)),
+	mRectTransform(nullptr)
+{
+	// Transformの二重生成を防ぐためにコンストラクタで生成して、アップキャストしたものを取得する
+	mRectTransform = dynamic_cast<RectTransform*>(mTransform.get());
+}
---
+++ Sources/World/UI/UIObject.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../GameObject.h"
+
+class RectTransform;
+
+/// <summary>
+/// RectTransformを持ったUIオブジェクトの基底
+/// </summary>
+class UIObject : public GameObject
+{
+public:
+
+	UIObject();
+	virtual ~UIObject() = default;
+
+	RectTransform* GetRectTransform() const { return mRectTransform; }
+
+protected:
+
+	RectTransform* mRectTransform;
+};
