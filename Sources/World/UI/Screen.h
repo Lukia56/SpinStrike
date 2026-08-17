10e859f808bc51e4ca7723c4aa700880f3085151
Lukia56 <kky56saqqe@gmail.com>
2026-08-17 10:19:27 +0900

    コールバック呼び出し処理修正

--- Sources/World/UI/Screen.h
+++ Sources/World/UI/Screen.h
@@ -21,7 +21,7 @@ namespace UI
 		Screen();
 		virtual ~Screen() = default;
 
-		virtual bool ProcessInput(const Input::UIInputState&) {};
+		virtual bool ProcessInput(const Input::UIInputState&) { return true; }
 
 		/// <summary>
 		/// コマンドを取得し、初期値へ戻す
