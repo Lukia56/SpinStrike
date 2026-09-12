0c61d85409ac1e26107127540a26412d39d6fe66
Lukia56 <kky56saqqe@gmail.com>
2026-09-13 01:15:56 +0900

    Update SceneStageTest.cpp

--- Sources/Scene/Test/SceneStageTest.cpp
+++ Sources/Scene/Test/SceneStageTest.cpp
@@ -1,4 +1,5 @@
 #include "SceneStageTest.h"
+#include <vector>
 #include "SceneSelectDebug.h"
 #include "World/Character/Player.h"
 #include "World/Character/PlayerBulletManager.h"
@@ -9,10 +10,15 @@
 #include "Camera/CameraManager.h"
 #include "System/Input/Keyboard.h"
 #include "System/Input/Mouse.h"
+#include "Utility/Data/JSON/JsonLoader.h"
 
 namespace
 {
 	const char* const kStageObjectDataPath = "Resources\\MasterData\\TestStage0.json";
+
+	const char* const kWaypointDataPath = "Resources\\MasterData\\TestStageWaypoint0.json";
+
+	const char* const kPatrollingDataPath = "Resources\\MasterData\\TestEnemyMoveData.json";
 }
 
 SceneStageTest::SceneStageTest() :
@@ -34,9 +40,15 @@ void SceneStageTest::OnInit()
 	mPlayer = objectRoot->CreateToChild<Player>(bulletManager);
 	mPlayer->GetTransform()->localPosition.y = 200.0f;
 
-	mEnemy = objectRoot->CreateToChild<Enemy>(mPlayer->GetTransform());
+
+	std::vector<WaypointGroup> waypointGroups = Data::Json::LoadJsonAs<WaypointGroup>(kWaypointDataPath);
+
+	std::vector<EnemyPatrollingData> patrollingData = std::move(Data::Json::LoadJsonAs<EnemyPatrollingData>(kPatrollingDataPath));
+
+	mEnemy = objectRoot->CreateToChild<Enemy>(mPlayer->GetTransform(), std::move(patrollingData[0]), waypointGroups);
 	mEnemy->GetTransform()->localPosition = Vector3(0.0f, 100.0f, 0.0f);
-	
+
+
 	objectRoot->CreateToChild<StageManager>(kStageObjectDataPath);
 
 	GetCameraManager()->AddCamera(Camera::Type::Follow, std::make_unique<CameraFollow>(mPlayer->GetTransform()));
